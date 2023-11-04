66d19395567af4ce7506710138a63aa44dfb9025
Andrea Riley <andreariley@me.com>
2023-11-04 15:29:47 -0400

    got code doesn't work fix later

--- app.js
+++ app.js
@@ -4,11 +4,11 @@
 // estimate sales with random in range function
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
-const seattleLocation = new CookieStore('Seattle', 23, 65, 6.3);
-const dubaiLocation = new CookieStore('Dubai', 11,38, 2.3)
-const tokyoLocation = new CookieStore('Tokyo', 3, 24, 1.2);
-const parisLocation = new CookieStore('Paris', 2, 16, 4.6);
-const limaLocation = new CookieStore('Lima', 2, 16, 4.6);
+const seattleLocation = new CookieStore('Seattle', 23, 65, 6.3, '6am-7pm', '2901 3rd Ave, Seattle WA', '(206) 575-1086');
+const dubaiLocation = new CookieStore('Dubai', 11,38, 2.3,'6am-7pm', '11, Manama Street - Dubai','55 631 9590');
+const tokyoLocation = new CookieStore('Tokyo', 3, 24, 1.2,'6am-7pm', '1 Chome-7804 Daigakuminami,	新潟市, 025-262-3550');
+const parisLocation = new CookieStore('Paris', 2, 16, 4.6,'6am-7pm', '98 Rue Montorgueil, 75002 Paris', '01 45 08 92 93');
+const limaLocation = new CookieStore('Lima', 2, 16, 4.6,'6am-7pm', 'Avenida Gran Chimú Avenida Gran Chimu cuadra 9, San Juan de Lurigancho, 036', '(01) 4890318,');
 
 
 const locations = [seattleLocation, tokyoLocation, dubaiLocation, parisLocation, limaLocation]
@@ -32,12 +32,15 @@ function estimateSales(store) {
 
 // cookie store constructor
 
-function CookieStore(store, minCust, maxCust, aveCookies){
+function CookieStore(store, minCust, maxCust, aveCookies, hours, address, contactInfo){
   this.store = store,
   this.minCust = minCust,
   this.maxCust = maxCust,
   this.aveCookies = aveCookies,
   this.salesArray = estimateSales(this);
+  this.hours = hours;
+  this.address = address;
+  this.contactInfo = contactInfo;
 }
 // estimate method as prototype method
 CookieStore.prototype.estimateSales = function() {
@@ -45,7 +48,7 @@ CookieStore.prototype.estimateSales = function() {
 }
 
 
-// create rows for each city with cookie data
+// create table and rows for each city with cookie data
 const tableElem = document.getElementById('data-table');
 
 let locationsHourlyTotal = 0;
@@ -76,9 +79,9 @@ dailyTotalCell.textContent = `${dailyTotal} cookies`;
 locationsDailyTotal += dailyTotal;
 }
 
+//  Location Header Cell
 const container = document.getElementById('root')
 
-//  Location Header Cell
 function header() {
   const tableRow = document.createElement('tr')
   tableElem.appendChild(tableRow);
@@ -162,3 +165,21 @@ limaLocation.render();
 renderfooterRow(tableElem);
 
 
+// location info on main page
+const section = document.getElementByID('locationsList');
+
+for (let i = 0; i < locations.length; i++) {}
+  const location = locations[i]
+ 
+  const locationDiv = document.createElement('div');
+  section.appendChild(locationDiv);
+  locationDiv.className = 'locationsList';
+
+  const nameElement = document.createElement('h2');
+  locationDiv.appendChild(nameElement);
+  nameElement.textContent = location.store;
+
+  const descriptionElement = document.createElement('p');
+  locationDiv.appendChild(descriptionElement);
+  descriptionElement.textContent = `Address: ${location.address}`, `Hours: ${location.hours}`, `Phone Number: ${location.contactInfo};
+}
